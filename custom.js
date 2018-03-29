6f01692ee8d20dd302bbcaf6719869356c1e7cd6
青木秀仁 <shamrock1976@me.com>
2018-03-29 21:54:24 +0900

    機能拡張をしました２

--- custom.js
+++ custom.js
@@ -1,3 +1,7 @@
+// Events functions
+//
+//
+
 // onInitialLatLng : This function is called when setting initial lat & lng.
 function onInitialLatLng() {
     return [35.737841　, 139.653912];
@@ -7,36 +11,6 @@ function onInitialLatLng() {
 function onCreate(map) {
     L.marker([35.737841　, 139.653912]).addTo(map)
     .bindPopup('練馬駅');
-}
-
-function createContent(feature) {
-    var popupContents = '<h4>練馬たぬきマップ</h4>';
- 
-    if (feature && feature.properties) {     
-        if (feature.properties.picture) {
-            popupContents += "<image src=\"./images/" + feature.properties.picture + "\" width=\"200\" /><br />" ;					
-		}
-        
-        if (feature.properties.content) {
-            popupContents += feature.properties.content ;
-        } else {
-            popupContents += "-" ;
-        }
-        
-        if (feature.properties.type) {
-            popupContents += "<br />" + feature.properties.type;
-        }
-        
-        if (feature.properties.size) {
-            popupContents += "<br />" + feature.properties.size;
-        }
-        
-        if (feature.properties.memo) {
-            popupContents += "<br />" + feature.properties.memo;
-        }
-    }
-
-    return popupContents ;
 }
 
 // onCreateMarkerIcon : This function is called when marker icon is created.
