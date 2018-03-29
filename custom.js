fe444f454805d4bd9b798b8a27983bd17d3f19d7
青木秀仁 <shamrock1976@me.com>
2018-03-29 21:53:48 +0900

    機能拡張をしました。

--- custom.js
+++ custom.js
@@ -9,35 +9,18 @@ function onCreate(map) {
     .bindPopup('練馬駅');
 }
 
-// onCreateMarkerIcon : This function is called when marker icon is created.
-function onCreateMarkerIcon() {
-    return L.icon({
-        iconUrl: 'tanuki_icon.png',
-        iconSize: [40, 40],
-        iconAnchor: [0, 0],
-        popupAnchor: [20, 0],
-        shadowUrl: '',
-        shadowSize: [38, 38],
-        shadowAnchor: [0, 0]
-    });
-}
-
-// onRequireAreaBorder : This function is called when loading area border geoJSON.
-function onRequireAreaBorder() {
-    return './data/13120.json';
-}
-
-// onCraeteMarkerPopup : This function is called when marker is created by Leaflet.
-function onCraeteMarkerPopup(feature, layer) {
-    if (feature.properties) {
-        var popupContents ;
+function createContent(feature) {
+    var popupContents = '<h4>練馬たぬきマップ</h4>';
+ 
+    if (feature && feature.properties) {     
+        if (feature.properties.picture) {
+            popupContents += "<image src=\"./images/" + feature.properties.picture + "\" width=\"200\" /><br />" ;					
+		}
         
-        if (feature.properties.content && feature.properties.picture) {
-            popupContents = "<a href=\"./images/" + feature.properties.picture + "\" target=\"_preview\">" + feature.properties.content + "</a>" ;					
-        } else if (feature.properties.content) {
-            popupContents = feature.properties.content ;
+        if (feature.properties.content) {
+            popupContents += feature.properties.content ;
         } else {
-            popupContents = "-" ;
+            popupContents += "-" ;
         }
         
         if (feature.properties.type) {
@@ -51,19 +34,55 @@ function onCraeteMarkerPopup(feature, layer) {
         if (feature.properties.memo) {
             popupContents += "<br />" + feature.properties.memo;
         }
-        
-        layer.bindPopup(popupContents);
     }
+
+    return popupContents ;
 }
 
+// onCreateMarkerIcon : This function is called when marker icon is created.
+function onCreateMarkerIcon() {
+    return L.icon({
+        iconUrl: 'tanuki_icon.png',
+        iconSize: [40, 40],
+        iconAnchor: [0, 0],
+        popupAnchor: [20, 0],
+        shadowUrl: '',
+        shadowSize: [38, 38],
+        shadowAnchor: [0, 0]
+    });
+}
+
+// onMapClick : This function is called when map was clicked.
+function onMapClick(e) {
+	//popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
+}
+
+// onRequireAreaBorder : This function is called when loading area border geoJSON.
+function onRequireAreaBorder() {
+    return './data/13120.json';
+}
+
+// onCraeteMarkerPopup : This function is called when marker is created by Leaflet.
+function onCraeteMarkerPopup(feature, layer) {
+    return createContent(feature);
+}
+
+// onMarkerMouseOver : This function is called marker mouse over. If return false, onUpdateInfo isn't called.
 function onMarkerMouseOver(e) {
-
+    return true ;
 }
 
+// onMarkerMouseOver : This function is called marker mouse out.
 function onMarkerMouseOut(e) {
-
+    
 }
 
+// onMarkerMouseOver : This function is called marker mouse click.
 function onMarkerClick(e) {
+    
+}
 
+// onUpdateInfo : This function is updated info content.
+function onUpdateInfo(feature) {
+    return createContent(feature);
 }
